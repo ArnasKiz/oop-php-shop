27341ec0229b6e117fe571c7f0676b5571093bcc
Arnas Kizela <112963622+ArnasKiz@users.noreply.github.com>
2022-10-12 18:59:54 +0300

    1 ir 2 Praktiniai

--- product.php
+++ product.php
@@ -1,8 +1,66 @@
 <?php
 
-require_once 'classes/Product.php';
+require_once('./classes/Product.php');
 
 // How to load data
-// Product::getProducts('./data/products.json')
+//Product::getProducts('./data/products.json');
+$product = null;
+if (isset($_GET['id'])) {
+    $product = Product::find($_GET['id']); // 2 dalis
+}
+?>
 
-// TODO: add product page
+
+<html>
+
+<head>
+    <title>
+        <?php if ($product != null) {
+            echo $product->title;
+        } else {
+            echo "Produktas nerastas";
+        } ?>
+    </title>
+</head>
+
+
+<body>
+    <?php if ($product != null) : ?>
+        <div>
+            <?php foreach ($product->images as $image) : ?>
+                <img src="<?php echo $image ?>" width="420" height="420">
+            <?php endforeach; ?>
+        </div>
+        <div>
+            <?php echo "Produkto ID: " . $product->id; ?>
+        </div>
+        <div>
+            <?php echo "Kurejas: " . $product->maker; ?>
+        </div>
+        <div>
+            <?php echo "Tinklapis: " . $product->url; ?>
+        </div>
+        <div>
+            <?php echo "Pavadinimas: " . $product->title; ?>
+        </div>
+        <div>
+            <?php echo "Aprasymas: " . $product->description; ?>
+        </div>
+        <div>
+            <?php echo "Kaina: " . $product->price; ?> EUR
+        </div>
+        <div>
+            Ivertinimas:
+            <?php
+            if ($product->avg_rating) {
+                echo $product->avg_rating;
+            } else {
+                echo "Nera duomenu";
+            }
+            ?>
+        </div><br>
+    <?php else : echo "Produktas nerastas."; ?>
+    <?php endif; ?>
+</body>
+
+</html>
--- products.php
+++ products.php
@@ -1,18 +1,46 @@
 <?php
 
-// require_once('./classes/Product.php');
+require_once('./classes/Product.php');
 
-// TODO: add products list page
-// $products = Product::getProducts('./data/products.json');
+$products = Product::getProducts('./data/products.json');
 ?>
 
 <html>
-  <head>
-    <title>
-      Products list
-    </title>
-  </head>
-  
-  <body>
-  </body>
+
+<head>
+  <title>
+    Produktu Sarasas
+  </title>
+</head>
+
+<body>
+
+  <div>
+    <?php foreach ($products as $product) : ?>
+      <span>
+        <div>
+          <a href="<?php echo "./product.php?id=" . $product->id ?>"><img src="<?php echo $product->images[0] ?>" width="420" height="420"></a>
+        </div>
+        <div>
+          <b><?php echo $product->title; ?></b>
+        </div>
+        <div>
+          <?php echo $product->price; ?> EUR
+        </div>
+        <div>
+          Ivertinimas:
+          <?php
+          if ($product->avg_rating) {
+            echo $product->avg_rating;
+          } else {
+            echo "Nera duomenu";
+          }
+          ?>
+        </div><br>
+      </span>
+    <?php endforeach; ?>
+  </div>
+
+</body>
+
 </html>
